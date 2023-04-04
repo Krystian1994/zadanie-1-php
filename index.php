c5f2f1a4601e91c397aca5cedcb903b6490b10b9
Krystian <krystian.surowiec.programista@gmail.com>
2023-04-04 15:06:52 +0200

    Begining of the task1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pzypomnienie php</title>
+</head>
+<body>
+<table>
+class Pipeline{
+    public function make(){
+        
+    }
+}
+
+</body>
+</html>
